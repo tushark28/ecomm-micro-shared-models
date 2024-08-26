6012420eb2abfb8b3e5eed266c0d34196eb8f07c
tushar kamboj <tushar.kamboj@binmile.com>
2024-08-26 12:32:13 +0530

    Improvements: Created index.ts for easier import

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { CreateUserDto } from "dto/create-user.dto";
+import { UpdateUserDto } from "dto/update-user.dto";
+import { User } from "models/User";
+
+export {
+    User, UpdateUserDto, CreateUserDto
+}
